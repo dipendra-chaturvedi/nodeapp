78a8bdcd6100c35dfac212aae491cf268b990b72
Dipendra Chaturvedi <dipendra2k23@gmail.com>
2026-04-16 13:14:19 +0530

    Add initial project setup with user management views and dependencies
    - Created package.json with necessary dependencies: body-parser, ejs, express, mysql2.
    - Added views for user management:
      - add.ejs: Form to add a new user.
      - edit.ejs: Form to edit an existing user.
      - index.ejs: List of users with options to view, edit, and delete.
      - view.ejs: Display user details.

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+// DB connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',        // change later for hosting
+    password: '',
+    database: 'myapp'
+});
+
+db.connect(err => {
+    if (err) throw err;
+    console.log("DB Connected");
+});
+
+// routes
+app.get('/', (req, res) => {
+    db.query('SELECT * FROM users', (err, result) => {
+        res.render('index', { users: result });
+    });
+});
+
+app.get('/add', (req, res) => {
+    res.render('add');
+});
+
+app.post('/add', (req, res) => {
+    const { name, email } = req.body;
+    db.query('INSERT INTO users (name,email) VALUES (?,?)', [name, email], () => {
+        res.redirect('/');
+    });
+});
+
+app.get('/view/:id', (req, res) => {
+    db.query('SELECT * FROM users WHERE id=?', [req.params.id], (err, result) => {
+        res.render('view', { user: result[0] });
+    });
+});
+
+app.get('/edit/:id', (req, res) => {
+    db.query('SELECT * FROM users WHERE id=?', [req.params.id], (err, result) => {
+        res.render('edit', { user: result[0] });
+    });
+});
+
+app.post('/edit/:id', (req, res) => {
+    const { name, email } = req.body;
+    db.query('UPDATE users SET name=?, email=? WHERE id=?',
+        [name, email, req.params.id],
+        () => res.redirect('/')
+    );
+});
+
+app.get('/delete/:id', (req, res) => {
+    db.query('DELETE FROM users WHERE id=?', [req.params.id], () => {
+        res.redirect('/');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
